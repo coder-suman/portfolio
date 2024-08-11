aa8d1be79ac7f40123b193ef8eb3a30715250c0b
Suman Bajani <128512864+coder-suman@users.noreply.github.com>
2024-08-11 22:13:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".multiple-text",{
+    strings: ["Frontend Developer","Software Developer","Programmer"],
+    typeSpeed: 100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
